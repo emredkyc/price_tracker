07c470bc98bece433b2e540b8e74cf649692b5be
emredkyc <emredkyc@hotmail.com>
2024-01-12 17:33:06 +0100

    add css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'], weight: ['300', '400', '500', '600']
+ })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
