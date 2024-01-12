eafd84c4f396dfd72463e4d3fd80d23f06f3c1da
emredkyc <emredkyc@hotmail.com>
2024-01-12 22:58:40 +0100

    create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> PriceTracker</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+
+          </div>
+
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home
