2fe8947757254fa0071ae3ebf5d8c6ac9cf94e61
emredkyc <emredkyc@hotmail.com>
2024-01-18 02:02:22 +0100

    create getSimilarProducts function

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -72,3 +72,21 @@ export async function getAllProducts() {
     console.log(error);
   }
 }
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if (!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+}
