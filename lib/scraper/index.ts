98220670eaeed08815d98d96143b41fe9fbef91b
emredkyc <emredkyc@hotmail.com>
2024-03-01 23:23:20 +0100

    add discountRate

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import axios from "axios";
 import * as cheerio from "cheerio";
@@ -56,7 +56,7 @@ export async function scrapeAmazonProduct(url: string) {
     const imageUrls = Object.keys(JSON.parse(images));
 
     const currency = extractCurrency($(".a-price-symbol"));
-    // const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
 
     const description = extractDescription($);
 
@@ -69,7 +69,7 @@ export async function scrapeAmazonProduct(url: string) {
       currentPrice: Number(currentPrice) || Number(originalPrice),
       originalPrice: Number(originalPrice) || Number(currentPrice),
       priceHistory: [],
-    //   discountRate: Number(discountRate),
+      discountRate: Number(discountRate),
       category: "category",
       reviewsCount: 100,
       stars: 4.5,
