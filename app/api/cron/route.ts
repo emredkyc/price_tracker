f58447fdc02e86f0c255cd7ae34d17a7467ae267
emredkyc <emredkyc@hotmail.com>
2024-03-01 23:53:29 +0100

    update mongoose name

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
-import { connectToDB } from "@/lib/moongose";
+import { connectToDB } from "@/lib/mongoose";
 import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
-import { connectToDB } from "../moongose";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
